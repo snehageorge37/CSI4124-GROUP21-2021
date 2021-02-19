9108645f048ccb980db93a718c2b6e773e75c97b
toluwani <ogunsanyatoluwani@gmail.com>
2021-02-19 18:34:07 -0500

    fix season in dates

--- dates.py
+++ dates.py
@@ -5,6 +5,7 @@ y = 2020
 m = 4
 d = 1
 
+summer_start = datetime.datetime(year=2020, month=6, day=20)
 holidays = ['10/04/20', '13/04/20', '23/04/20', '18/05/20', '01/07/20']
 dates = []
 frame = [['id', 'day', 'month', 'year', 'season', 'isWeekend', 'isHoliday']]
@@ -17,7 +18,7 @@ while y <= 2020:
             try:
                 date = datetime.datetime(year=y, month=m, day=d)
                 print(date.strftime('%d/%m/%y'))
-                frame.append([counter, d, m, y, 'summer', not date.weekday(), date.strftime('%d/%m/%y') in holidays])
+                frame.append([counter, d, m, y, 'summer' if date >= summer_start else 'spring', not date.weekday(), date.strftime('%d/%m/%y') in holidays])
                 d += 1
                 counter += 1
             except:
